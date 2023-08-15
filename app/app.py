a9ea565ce74f007bff8e0a4c0b1ce851638dcf4a
aldrich <jslilin987654321@gmail.com>
2023-08-15 17:51:56 +0800

    feat: fix bugs

--- app/app.py
+++ app/app.py
@@ -4,9 +4,12 @@ import os
 
 import aiofiles
 import aiohttp
-from flask import Flask, render_template
+import requests
+from flask import Flask, render_template, request, Response
 
 app = Flask(__name__)
+
+result_urls = None
 
 total = 0
 
@@ -18,22 +21,83 @@ async def render_html():
     return render_template('template.html', data={'list': data, 'total': len(data), 'allow': total})
 
 
-@app.route("/test")
+@app.route('/draw', methods=['GET', 'POST'])
+def draw():
+    if request.method == 'GET':
+        return render_template('draw.html', data={'images': [], 'prompt': ''})
+    else:
+        prompt = request.form['prompt']
+        negative_prompt = request.form['negative_prompt']
+        n_iter = request.form['n_iter']
+        width = request.form['width']
+        height = request.form['height']
+        steps = request.form['steps']
+        url = request.args.get('url')
+
+        headers = {"Content-Type": "application/json"}
+        request_json = {
+            "prompt": prompt,
+            "negative_prompt": negative_prompt,
+            "n_iter": int(n_iter) if n_iter else 12,
+            "width": int(width) if width else 600,
+            "height": int(height) if height else 800,
+            "steps": int(steps) if steps else 30,
+        }
+
+        print(f"URL: {url}")
+        print(f"Prompt: {prompt}")
+        print(f"Negative Prompt: {negative_prompt}")
+        print("开始绘制,等待中......")
+
+        is_ajax_request = request.headers.get('X-Requested-With') == 'XMLHttpRequest'
+
+        try:
+            response = requests.post(url + '/sdapi/v1/txt2img', json=request_json, headers=headers)
+            json_data = response.json()
+            images = json_data.get("images", [])
+
+            if is_ajax_request:
+                return Response(images, mimetype='application/json')
+            else:
+                return render_template('draw.html', data={'images': images, 'prompt': prompt})
+        except Exception:
+            pass
+        if is_ajax_request:
+            return Response([], mimetype='application/json')
+        else:
+            return render_template('draw.html', data={'images': [], 'prompt': ''})
+
+
+@app.route("/test", methods=['GET', 'POST'])
 async def test_html():
-    data = await read_json_files()
-    timeout = aiohttp.ClientTimeout(sock_read=60)
+    global result_urls
 
-    async with aiohttp.ClientSession(timeout=timeout) as session:
-        print('-----------当前可用域名------------')
-        tasks = []
-        for item in data:
-            task = asyncio.create_task(scan_port(session, item))
-            tasks.append(task)
-        result = await asyncio.gather(*tasks)
-        print(f'-----------结束 {total}/{len(data)}------------')
+    # if request.method == 'GET':
+    #     return render_template('template.html', data={'list': [], 'total': 0, 'allow': 0})
 
+    if result_urls is None:
+        print("当前没有验证,重新获取喽!!!!")
+        data = await read_json_files()
+        timeout = aiohttp.ClientTimeout(sock_read=60)
+
+        async with aiohttp.ClientSession(timeout=timeout) as session:
+            print('-----------当前可用域名------------')
+            tasks = []
+            for item in data:
+                task = asyncio.create_task(scan_port(session, item))
+                tasks.append(task)
+            result_urls = await asyncio.gather(*tasks)
+            print(f'-----------结束 {total}/{len(data)}------------')
+
+    is_ajax_request = request.headers.get('X-Requested-With') == 'XMLHttpRequest'
+
+    print(request.headers)
+    if is_ajax_request:
+        result = json.dumps(result_urls)
+        return Response(result, mimetype='application/json')
     # 渲染模板并返回HTML响应
-    return render_template('template.html', data={'list': result, 'total': len(result), 'allow': total})
+    else:
+        return render_template('template.html', data={'list': result_urls, 'total': len(result_urls), 'allow': total})
 
 
 async def read_json_files():
@@ -54,7 +118,7 @@ async def scan_port(session, item):
     try:
         headers = {'Content-Type': 'application/json'}
         async with session.post(api, data=json.dumps({
-            "prompt": "1girl,loli,black bodysuit,see-through,covered navel,spread legs,",
+            "prompt": "loli,little girl,pettie,black bodysuit,see-through,covered nipples,",
             "negative_prompt": "(worst quality:1.3),(low quality:1.3),(normal quality:1.3),",
             # "height": 768
         }), headers=headers) as response:
