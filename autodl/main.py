c8f8dda30c88cd6ea3589b01d97b86b200be06a8
aldrich <jslilin987654321@gmail.com>
2023-09-17 17:59:28 +0800

    feat: fix bugs

--- autodl/main.py
+++ autodl/main.py
@@ -35,16 +35,22 @@ async def scan_port(session, server, port, batch_progress_bar):
         async with session.get(url) as response:
             if 200 <= response.status < 300:
                 html = await response.text()
-                if 'Stable' in html:
-                    pattern = re.compile(r'(?<=")[^"]*\.safetensors[\s\[\]a-z0-9]*(?=")')
+                if "Stable" in html:
+                    pattern = re.compile(
+                        r'(?<=")[^"]*\.safetensors[\s\[\]a-z0-9]*(?=")'
+                    )
                     result = pattern.findall(html)
                     temp = []
                     for item in result:
-                        if item not in temp and 'lora/' not in item and 'Lora/' not in item:
+                        if (
+                            item not in temp
+                            and "lora/" not in item
+                            and "Lora/" not in item
+                        ):
                             temp.append(item)
                     online.append({"url": url, "models": temp})
-    except aiohttp.ClientError:
-        pass
+    except aiohttp.ClientError as e:
+        raise e
     except asyncio.TimeoutError:
         pass
     except Exception:
@@ -53,12 +59,17 @@ async def scan_port(session, server, port, batch_progress_bar):
 
 
 async def main():
-    timeout = aiohttp.ClientTimeout(sock_read=20)
+    timeout = aiohttp.ClientTimeout(sock_read=15)
     ports_to_scan = range(10000, 65536)
     batch_size = 3000
     total_batches = (len(ports_to_scan) - 1) // batch_size + 1
-    total_progress_bar = tqdm(total=len(ports_to_scan) * len(servers), desc="Overall Progress", unit="port",
-                              position=0, leave=True)
+    total_progress_bar = tqdm(
+        total=len(ports_to_scan) * len(servers),
+        desc="Overall Progress",
+        unit="port",
+        position=0,
+        leave=True,
+    )
 
     async with aiohttp.ClientSession(timeout=timeout) as session:
         for server in servers:
@@ -67,18 +78,28 @@ async def main():
                 end_idx = min((batch + 1) * batch_size, len(ports_to_scan))
 
                 batch_ports = ports_to_scan[start_idx:end_idx]
-                batch_progress_bar = tqdm(total=len(batch_ports),
-                                          desc=f"Scanning {server} Ports - Batch {batch + 1}/{total_batches}",
-                                          unit="port",
-                                          position=0, leave=True)
+                batch_progress_bar = tqdm(
+                    total=len(batch_ports),
+                    desc=f"Scanning {server} Ports - Batch {batch + 1}/{total_batches}",
+                    unit="port",
+                    position=0,
+                    leave=True,
+                )
 
-                tasks = [scan_port(session, server, port, batch_progress_bar) for port in batch_ports]
-                await asyncio.gather(*tasks)
+                tasks = [
+                    scan_port(session, server, port, batch_progress_bar)
+                    for port in batch_ports
+                ]
+
+                try:
+                    await asyncio.gather(*tasks, return_exceptions=True)
+                except Exception:
+                    break
 
                 batch_progress_bar.close()
                 total_progress_bar.update(len(batch_ports))
 
-                sleep_time = 6 + batch % 3
+                sleep_time = 5 + batch % 3
                 await asyncio.sleep(sleep_time)
 
                 if end_idx >= 55536:
@@ -86,12 +107,12 @@ async def main():
 
     total_progress_bar.close()
 
-    file_path = pathlib.Path(f'./global_scan_result_{time.time()}.json')
+    file_path = pathlib.Path(f"./global_scan_result_{time.time()}.json")
 
     if len(servers) == 1:
-        file_path = pathlib.Path(f'./{servers[0]}_scan_result_{time.time()}.json')
+        file_path = pathlib.Path(f"./{servers[0]}_scan_result_{time.time()}.json")
 
-    with file_path.open('w', encoding='u8') as fp:
+    with file_path.open("w", encoding="u8") as fp:
         json.dump(online, fp, ensure_ascii=False)
 
 
